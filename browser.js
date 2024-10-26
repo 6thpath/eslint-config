ce0bc17fa4b6daa725859e0edd6a44c2c2022fe3
Phát Mai <tienphat1501@gmail.com>
2024-10-27 02:51:30 +0700

    feat: update globals on some presets

--- browser.js
+++ browser.js
@@ -3,4 +3,4 @@ const globals = require('globals')
 const baseConfig = require('./base')
 
 /** @type {import('eslint').Linter.Config[]} */
-module.exports = [...baseConfig, { languageOptions: { globals: globals.browser } }]
+module.exports = [...baseConfig, { languageOptions: { globals: { ...globals.builtin, ...globals.browser } } }]
--- node.js
+++ node.js
@@ -3,4 +3,4 @@ const globals = require('globals')
 const baseConfig = require('./base')
 
 /** @type {import('eslint').Linter.Config[]} */
-module.exports = [...baseConfig, { languageOptions: { globals: globals.node } }]
+module.exports = [...baseConfig, { languageOptions: { globals: { ...globals.builtin, ...globals.node } } }]
